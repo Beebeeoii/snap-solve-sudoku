f791267c0029534c08ef0c028dec3ebb932b3336
Beebeeoii <b33b33o11@gmail.com>
2020-08-02 21:27:05 +0800

    null

--- app/src/main/java/com/example/snapsolvesudoku/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/snapsolvesudoku/fragments/SettingsFragment.kt
@@ -8,5 +8,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
     }
 }
