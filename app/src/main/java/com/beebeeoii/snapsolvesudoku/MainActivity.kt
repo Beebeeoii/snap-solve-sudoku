05b2588f2d905b77cc2d270b5d789503e3821e22
Beebeeoii <b33b33o11@gmail.com>
2020-08-21 13:30:08 +0800

    Robust permissions

--- app/src/main/java/com/beebeeoii/snapsolvesudoku/MainActivity.kt
+++ app/src/main/java/com/beebeeoii/snapsolvesudoku/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.beebeeoii.snapsolvesudoku
 
-import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
-import java.io.*
 
 private const val TAG = "MainActivity"
 
@@ -28,45 +25,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        ActivityCompat.requestPermissions(this, arrayOf(
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-
-        copyModelData()
-
-    }
-
-    private fun copyModelData() {
-        val modelFileName = "070820090621"
-        val modelPath = "${getExternalFilesDir(null).toString()}/model"
-        try {
-            val dir = File(modelPath)
-
-            if (!dir.exists()) {
-                dir.mkdirs()
-            } else {
-                return
-            }
-
-            val pathToDataFile = "${modelPath}/${modelFileName}"
-            if (!File(pathToDataFile).exists()) {
-                val `in` = assets.open(modelFileName)
-                val out = FileOutputStream(pathToDataFile)
-                val buffer = ByteArray(1024)
-                var read = `in`.read(buffer)
-                while (read != -1) {
-                    out.write(buffer, 0, read)
-                    read = `in`.read(buffer)
-                }
-                `in`.close()
-                out.flush()
-                out.close()
-            }
-        } catch (exception : Exception) {
-            exception.printStackTrace()
-        }
     }
 
     override fun onResume() {
