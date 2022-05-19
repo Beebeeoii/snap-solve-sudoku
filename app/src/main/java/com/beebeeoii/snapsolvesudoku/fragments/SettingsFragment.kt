bc6090757dd4b7ff9b09ab06e15f0891fc9dbe6f
beebeeoii <b33b33o11@gmail.com>
2022-05-19 19:48:46 +0800

    ui(Settings): modified settings page

--- app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/SettingsFragment.kt
+++ app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/SettingsFragment.kt
@@ -1,13 +1,10 @@
 package com.beebeeoii.snapsolvesudoku.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentContainerView
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.beebeeoii.snapsolvesudoku.preferences.Preferences
 import com.beebeeoii.snapsolvesudoku.R
@@ -34,23 +31,6 @@ class SettingsFragment : Fragment() {
             val action = SettingsFragmentDirections.actionSettingsFragmentPop()
             findNavController().navigate(action)
         }
-
-//        appBar = rootView.findViewById(R.id.appBar)
-//
-//        appBar.setNavigationOnClickListener {
-//            requireActivity().onBackPressed()
-//        }
-//
-//        appBar.setOnMenuItemClickListener {
-//            when (it.itemId) {
-//                R.id.about -> {
-//                    val action = SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
-//                    findNavController().navigate(action)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
 
         return binding.root
     }
