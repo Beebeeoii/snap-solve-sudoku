b63cad430e59e9f5ecaac8f42b60b1d4b2671adc
jiawei <b33b33o11@gmail.com>
2022-06-17 22:19:08 +0800

    chore: removed unused dependencies

--- app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/MainFragment.kt
+++ app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/MainFragment.kt
@@ -1,8 +1,6 @@
 package com.beebeeoii.snapsolvesudoku.fragments
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
