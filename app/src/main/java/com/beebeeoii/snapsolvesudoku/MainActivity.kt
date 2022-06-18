c7008a0977265b21f44310cd08dba0c536a503de
jiawei <b33b33o11@gmail.com>
2022-06-18 14:12:09 +0800

    feat: enabled copying of model data

--- app/src/main/java/com/beebeeoii/snapsolvesudoku/MainActivity.kt
+++ app/src/main/java/com/beebeeoii/snapsolvesudoku/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
+import java.io.File
+import java.io.FileOutputStream
 
 private const val TAG = "MainActivity"
 
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        copyModelData()
     }
 
     @Override
@@ -35,6 +38,38 @@ class MainActivity : AppCompatActivity() {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback)
         } else {
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+
+    private fun copyModelData() {
+        // TODO Remove hardcoding of model file name
+        val modelFileName = "070820090621"
+        val modelPath = "${this.getExternalFilesDir(null).toString()}/model"
+        try {
+            val dir = File(modelPath)
+
+            if (!dir.exists()) {
+                dir.mkdirs()
+            } else {
+                return
+            }
+
+            val pathToDataFile = "${modelPath}/${modelFileName}"
+            if (!File(pathToDataFile).exists()) {
+                val `in` = this.assets.open(modelFileName)
+                val out = FileOutputStream(pathToDataFile)
+                val buffer = ByteArray(1024)
+                var read = `in`.read(buffer)
+                while (read != -1) {
+                    out.write(buffer, 0, read)
+                    read = `in`.read(buffer)
+                }
+                `in`.close()
+                out.flush()
+                out.close()
+            }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
         }
     }
 }
