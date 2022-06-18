2f31faf5aadd49c18cb3dcd95c9a6047d4f87510
jiawei <b33b33o11@gmail.com>
2022-06-18 13:10:09 +0800

    removed slide to delete and filter button for history

--- app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/HistoryFragment.kt
+++ app/src/main/java/com/beebeeoii/snapsolvesudoku/fragments/HistoryFragment.kt
@@ -1,31 +1,15 @@
 package com.beebeeoii.snapsolvesudoku.fragments
 
-import android.app.AlertDialog
-import android.content.DialogInterface
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.beebeeoii.snapsolvesudoku.R
 import com.beebeeoii.snapsolvesudoku.adapter.HistoryRecyclerAdapter
 import com.beebeeoii.snapsolvesudoku.databinding.FragmentHistoryBinding
 import com.beebeeoii.snapsolvesudoku.db.Database
 import com.beebeeoii.snapsolvesudoku.db.HistoryEntity
-import com.beebeeoii.snapsolvesudoku.utils.FileDeletor
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.File
-import kotlin.math.absoluteValue
 
 private const val TAG = "HistoryFragment"
 
@@ -59,119 +43,8 @@ class HistoryFragment : Fragment(){
 
         binding.historyRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        val itemTouchCallback: ItemTouchHelper.SimpleCallback =
-            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-                val deleteIcon = ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.ic_delete_forever_24px
-                )
-                val background = ColorDrawable(Color.RED)
-
-                override fun onChildDraw(
-                    c: Canvas,
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    dX: Float,
-                    dY: Float,
-                    actionState: Int,
-                    isCurrentlyActive: Boolean
-                ) {
-                    super.onChildDraw(
-                        c,
-                        recyclerView,
-                        viewHolder,
-                        dX,
-                        dY,
-                        actionState,
-                        isCurrentlyActive
-                    )
-
-                    val itemView = viewHolder.itemView
-
-                    val intrinsicHeight = deleteIcon?.intrinsicHeight
-                    val intrinsicWidth = deleteIcon?.intrinsicWidth
-                    val top = itemView.top + (itemView.height - intrinsicHeight!!) / 2
-                    val left = itemView.width - intrinsicWidth!! -
-                            (itemView.height - intrinsicHeight) / 5
-                    val right = left + intrinsicHeight
-                    val bottom = top + intrinsicHeight
-
-                    if (dX < 0) {
-                        var amountDragged = dX.toInt()
-                        if (amountDragged.absoluteValue >= itemView.width) {
-                            amountDragged = -itemView.width
-                        }
-                        background.setBounds(
-                            itemView.right + amountDragged,
-                            itemView.top,
-                            itemView.right,
-                            itemView.bottom
-                        )
-                        deleteIcon?.setBounds(left, top, right, bottom)
-                    }
-
-                    background.draw(c)
-                    deleteIcon?.draw(c)
-                }
-
-                override fun onMove(
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    target: RecyclerView.ViewHolder
-                ): Boolean {
-                    return false
-                }
-
-                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
-                    val itemAdapterPosition = viewHolder.adapterPosition
-                    val dialogBuilder = AlertDialog.Builder(requireContext())
-                    dialogBuilder.setCancelable(false)
-                    dialogBuilder.setTitle("Delete history")
-                    dialogBuilder.setMessage("This action cannot be undone.")
-                    dialogBuilder.setPositiveButton("Delete") { _: DialogInterface, _: Int ->
-                        val historyEntity = historyEntityList[itemAdapterPosition]
-                        CoroutineScope(Dispatchers.IO).launch {
-                            historyDao.deleteHistoryEntry(historyEntity)
-                        }
-
-                        FileDeletor.deleteFileOrDirectory(File(historyEntity.folderPath))
-                        binding.historyRecyclerView.adapter?.notifyItemRemoved(itemAdapterPosition)
-
-                        Snackbar.make(
-                            requireView(),
-                            "History deleted successfully!",
-                            Snackbar.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    dialogBuilder.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
-                        binding.historyRecyclerView.adapter?.notifyItemChanged(itemAdapterPosition)
-                    }
-
-                    dialogBuilder.create().show()
-                }
-        }
-
-        val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
-        itemTouchHelper.attachToRecyclerView(binding.historyRecyclerView)
-
         binding.appBar.setNavigationOnClickListener {
             requireActivity().onBackPressedDispatcher.onBackPressed()
-        }
-
-        binding.appBar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.sort -> {
-                    historyEntityList = historyEntityList.asReversed()
-                    binding.historyRecyclerView.adapter = HistoryRecyclerAdapter(
-                        historyEntityList,
-                        requireContext(),
-                        requireActivity()
-                    )
-                    true
-                }
-                else -> false
-            }
         }
 
         return binding.root
